1552741769b78fd7349f3f8b151433e35fb0fe8c
Cristian Jaimes <cristianfaryd.jaimes.contractor@bbva.com>
2024-04-12 21:28:23 -0500

    Entidades interaccion &  oportunidades

---
+++ Interacoop/InteraCoop.Shared/Entities/Interaction.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteraCoop.Shared.Entities
+{
+    public class Interaction
+    {
+
+        public int Id
+        { get; set; }
+
+        [Display(Name = "Identificador del cliente")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public int ClientIdentifier
+        { get; set; }
+
+        [Display(Name = "Código de usuario")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        public string UserCode
+        { get; set; } = null!;
+
+        [Display(Name = "Tipo de interacción")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string InteractionType
+        { get; set; } = null!;
+
+        [Display(Name = "Fecha de creación de la interacción")]
+        [DataType(DataType.Date)]
+        public DateTime InteractionCreationDate
+        { get; set; }
+
+        [Display(Name = "Fecha de inicio")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate
+        { get; set; }
+
+        [Display(Name = "Fecha fin")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate
+        { get; set; }
+
+        [Display(Name = "Dirección")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string Address
+        { get; set; } = null!;
+
+        [Display(Name = " Observaciones de la interacción")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string ObservationsInteraction
+        { get; set; } = null!;
+
+        [Display(Name = "Oficina")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string Office
+        { get; set; } = null!;
+
+        [Display(Name = "Fecha de auditoria")]
+        [DataType(DataType.Date)]
+        public DateTime AuditDate
+        { get; set; }
+
+        [Display(Name = "Usuario de auditoría")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string AuditUser
+        { get; set; } = null!;
+    }
+}
