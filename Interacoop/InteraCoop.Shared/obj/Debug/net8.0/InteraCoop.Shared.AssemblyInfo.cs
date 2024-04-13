3c37cc9a0a859e2a6c439b1b9d5e2e4fcc558f69
andres-castillo17 <andres.castillo@itstk.com>
2024-04-12 20:45:19 -0500

    Creacion de entidades Oportunidad y Interacción

--- Interacoop/InteraCoop.Shared/obj/Debug/net8.0/InteraCoop.Shared.AssemblyInfo.cs
+++ Interacoop/InteraCoop.Shared/obj/Debug/net8.0/InteraCoop.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InteraCoop.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc1fe91b6766cb0419d8a4f816fb59df41ea2f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+909bf58e2c1bd3445820f0632674f31b0ad662c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("InteraCoop.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InteraCoop.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
