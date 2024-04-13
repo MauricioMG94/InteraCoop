b8d05c68a7d2ba69b830f791ca1d0aed1f433373
DESKTOP-T4RDMBF\Usuario <haprorold@gmail.com>
2024-04-13 06:29:34 -0500

    Se crea la entidad pais y su controller

--- Interacoop/InteraCoop.Backend/Program.cs
+++ Interacoop/InteraCoop.Backend/Program.cs
@@ -1,3 +1,6 @@
+using InteraCoop.Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
+app.UseCors(x => x
+.AllowAnyMethod()
+.AllowAnyHeader()
+.SetIsOriginAllowed(origin => true)
+.AllowCredentials());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
