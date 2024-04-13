d3a7c6830d65f68c220978ff4ac1795735b78d1a
Mauricio Martinez <ivanm.martinezg@gmail.com>
2024-04-12 22:09:40 -0500

    Creación de REpositories y UnitsOfWork

---
+++ Interacoop/InteraCoop.Backend/UnitsOfWork/Implementations/GenericUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using InteraCoop.Backend.UnitsOfWork.Interfaces;
+using InteraCoop.Shared.Responses;
+
+namespace InteraCoop.Backend.UnitsOfWork.Implementations
+{
+    public class GenericUnitOfWork<T> : IGenericUnitOfWork<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repostory;
+
+        public GenericUnitOfWork(IGenericRepository<T> repository)
+        {
+            _repostory = repository;
+        }
+
+        public virtual async Task<ActionResponse<T>> AddAsync(T model) => await _repostory.AddAsync(model);
+        public virtual async Task<ActionResponse<T>> DeleteAsync(int id) => await _repostory.DeleteAsync(id);
+        public virtual async Task<ActionResponse<T>> GetAsync(int id) => await _repostory.GetAsync(id);
+        public virtual async Task<ActionResponse<IEnumerable<T>>> GetAsync() => await _repostory.GetAsync();
+        public virtual async Task<ActionResponse<T>> UpdateAsync(T model) => await _repostory.UpdateAsync(model);
+    }
+}
---
+++ Interacoop/InteraCoop.Backend/UnitsOfWork/Interfaces/IGenericUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using InteraCoop.Shared.Responses;
+
+namespace InteraCoop.Backend.UnitsOfWork.Interfaces
+{
+    public interface IGenericUnitOfWork<T> where T : class
+    {
+        Task<ActionResponse<T>> GetAsync(int id);
+        Task<ActionResponse<IEnumerable<T>>> GetAsync();
+        Task<ActionResponse<T>> AddAsync(T model);
+        Task<ActionResponse<T>> DeleteAsync(int id);
+        Task<ActionResponse<T>> UpdateAsync(T model);
+    }
+}
