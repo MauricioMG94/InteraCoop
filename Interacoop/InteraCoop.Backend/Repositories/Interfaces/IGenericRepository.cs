a709bc16c05308a1ffa170eee6b50480206879bd
Cristian Jaimes <cristianfaryd.jaimes.contractor@bbva.com>
2024-04-13 07:28:50 -0500

    unit of work

---
+++ Interacoop/InteraCoop.Backend/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using InteraCoop.Shared.Responses;
+
+namespace InteraCoop.Backend.UnitsOfWork.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<ActionResponse<T>> GetAsync(int id);
+        Task<ActionResponse<IEnumerable<T>>> GetAsync();
+        Task<ActionResponse<T>> AddAsync(T entity);
+        Task<ActionResponse<T>> DeleteAsync(int id);
+        Task<ActionResponse<T>> UpdateAsync(T entity);
+    }
+}
