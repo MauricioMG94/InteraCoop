ff667f7d71c21ef270d360dcab65ea49a01acd0a
Mauricio Martinez <ivanm.martinezg@gmail.com>
2024-04-16 14:53:26 -0500

    Creación de Repositorios y Unidad de trabajo para oportunidades, y otros cambios

---
+++ Interacoop/InteraCoop.Backend/UnitsOfWork/Implementations/OpportunitiesUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using InteraCoop.Backend.Repositories.Interfaces;
+using InteraCoop.Backend.UnitsOfWork.Interfaces;
+using InteraCoop.Shared.Dtos;
+using InteraCoop.Shared.Entities;
+using InteraCoop.Shared.Responses;
+
+namespace InteraCoop.Backend.UnitsOfWork.Implementations
+{
+    public class OpportunitiesUnitOfWork : GenericUnitOfWork<Opportunity>, IOpportunitiesUnitOfWork
+    {
+        private readonly IOpportunitiesRepository _opportunitiesRepository;
+
+        public OpportunitiesUnitOfWork(IGenericRepository<Opportunity> repository, IOpportunitiesRepository opportunitiesRepository) : base(repository)
+        {
+            _opportunitiesRepository = opportunitiesRepository;
+        }
+
+        public override async Task<ActionResponse<IEnumerable<Opportunity>>> GetAsync(PaginationDTO pagination) => await _opportunitiesRepository.GetAsync(pagination);
+        public override async Task<ActionResponse<int>> GetTotalPagesAsync(PaginationDTO pagination) => await _opportunitiesRepository.GetTotalPagesAsync(pagination);
+        public override async Task<ActionResponse<Opportunity>> GetAsync(int id) => await _opportunitiesRepository.GetAsync(id);
+        public async Task<ActionResponse<Opportunity>> AddAsync(OpportunityDto opportunityDto) => await _opportunitiesRepository.AddAsync(opportunityDto);
+        public async Task<ActionResponse<Opportunity>> UpdateAsync(OpportunityDto opportunityDto) => await _opportunitiesRepository.UpdateAsync(opportunityDto);
+        public override async Task<ActionResponse<Opportunity>> DeleteAsync(int id) => await _opportunitiesRepository.DeleteAsync(id);
+    }
+}
---
+++ Interacoop/InteraCoop.Shared/Dtos/OpportunityDto.cs
@@ -0,0 +1,35 @@
+﻿using InteraCoop.Shared.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace InteraCoop.Shared.Dtos
+{
+    public class OpportunityDto
+    {
+        public int Id
+        { get; set; }
+
+        [Display(Name = "Estado")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string Status
+        { get; set; } = null!;
+
+        [Display(Name = " Observaciones de la oportunidad")]
+        [Required(ErrorMessage = "El campo {0} es requerido.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]
+        public string OpportunityObservations
+        { get; set; } = null!;
+
+        [Display(Name = "Fecha de registro")]
+        [DataType(DataType.Date)]
+        public DateTime RecordDate
+        { get; set; }
+
+        [Display(Name = "Fecha estimada de adquisición")]
+        [DataType(DataType.Date)]
+        public DateTime EstimatedAcquisitionDate
+        { get; set; }
+        public List<int>? InteractionsIds { get; set; }
+        public List<int>? CampaingsIds { get; set; }
+    }
+}
