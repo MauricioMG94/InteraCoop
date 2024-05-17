6aeffeffab870382ee634d6f9e868b56b5eee960
ANDRES CAMILO CASTILLO <castillocamilo17@gmail.com>
2024-05-16 20:44:11 -0500

    Editar Usuario dos

--- Interacoop/InteraCoop.Frontend/Pages/Auth/Login.razor.cs
+++ Interacoop/InteraCoop.Frontend/Pages/Auth/Login.razor.cs
@@ -1,3 +1,5 @@
+using Blazored.Modal.Services;
+using Blazored.Modal;
 using CurrieTechnologies.Razor.SweetAlert2;
 using InteraCoop.Frontend.Repositories;
 using InteraCoop.Frontend.Services;
@@ -9,13 +11,21 @@ namespace InteraCoop.Frontend.Pages.Auth
     public partial class Login
     {
         private LoginDto loginDto = new();
+        private bool wasClose;
         [Inject] private IRepository Repository { get; set; } = null!;
         [Inject] private NavigationManager NavigationManager { get; set; } = null!;
         [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
         [Inject] private ILoginService LoginService { get; set; } = null!;
-
+        
+       
         private async Task LoginAsync()
         {
+            //if (wasClose)
+            //{
+            //    NavigationManager.NavigateTo("/");
+            //    return;
+            //}
+
             var responseHttp = await Repository.PostAsync<LoginDto, TokenDto>("/api/accounts/Login", loginDto);
             if (responseHttp.Error)
             {
--- Interacoop/InteraCoop.Frontend/Pages/Auth/Register.razor.cs
+++ Interacoop/InteraCoop.Frontend/Pages/Auth/Register.razor.cs
@@ -2,6 +2,7 @@ using CurrieTechnologies.Razor.SweetAlert2;
 using InteraCoop.Frontend.Repositories;
 using InteraCoop.Frontend.Services;
 using InteraCoop.Shared.Dtos;
+using InteraCoop.Shared.Entities;
 using InteraCoop.Shared.Enums;
 using Microsoft.AspNetCore.Components;
 
@@ -10,6 +11,9 @@ namespace InteraCoop.Frontend.Pages.Auth
     public partial class Register
     {
         private UserDto userDto= new();
+        private List<Country>? countries;
+        private List<State>? states;
+        private List<City>? cities;
         private bool loading;
         private string? imageUrl;
 
@@ -18,13 +22,71 @@ namespace InteraCoop.Frontend.Pages.Auth
         [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
         [Inject] private ILoginService LoginService { get; set; } = null!;
 
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadCountriesAsync();
+        }
 
         private void ImageSelected(string imagenBase64)
         {
             userDto.Photo = imagenBase64;
             imageUrl = null;
         }
+        private async Task LoadCountriesAsync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Country>>("/api/countries/combo");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
 
+            countries = responseHttp.Response;
+        }
+
+        private async Task CountryChangedAsync(ChangeEventArgs e)
+        {
+            var selectedCountry = Convert.ToInt32(e.Value!);
+            states = null;
+            cities = null;
+            userDto.CityId = 0;
+            await LoadStatesAsyn(selectedCountry);
+        }
+
+        private async Task LoadStatesAsyn(int countryId)
+        {
+            var responseHttp = await Repository.GetAsync<List<State>>($"/api/states/combo/{countryId}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            states = responseHttp.Response;
+        }
+
+        private async Task StateChangedAsync(ChangeEventArgs e)
+        {
+            var selectedState = Convert.ToInt32(e.Value!);
+            cities = null;
+            userDto.CityId = 0;
+            await LoadCitiesAsyn(selectedState);
+        }
+
+        private async Task LoadCitiesAsyn(int stateId)
+        {
+            var responseHttp = await Repository.GetAsync<List<City>>($"/api/cities/combo/{stateId}");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+            }
+
+            cities = responseHttp.Response;
+        }
         private async Task CreateUserAsync()
         {
             userDto.UserName = userDto.Email;
