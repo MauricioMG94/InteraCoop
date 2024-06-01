87d87a1f5ec7075763afd8aaa4a20e1f2f7d1e32
MauricioMG94 <martinez.maurice94@gmail.com>
2024-06-01 18:07:49 -0500

    Se adicionan Test para genericos y campañas (Método GET).

--- Interacoop/InteraCoop.Tests/Controllers/CampaignsControllerTests.cs
+++ Interacoop/InteraCoop.Tests/Controllers/CampaignsControllerTests.cs
@@ -1,12 +1,170 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InteraCoop.Backend.Controllers;
+using InteraCoop.Backend.UnitsOfWork.Interfaces;
+using InteraCoop.Shared.Dtos;
+using InteraCoop.Shared.Entities;
+using InteraCoop.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
 
 namespace InteraCoop.Tests.Controllers
 {
-    internal class CampaignsControllerTests
+    [TestClass]
+    public class CampaignsControllerTests
     {
+        private Mock<IGenericUnitOfWork<Campaign>> _mockGenericUnitOfWork = null!;
+        private Mock<ICampaignsUnitOfWork> _mockCampaignsUnitOfWork = null!; 
+        private CampaignsController _controller = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<Campaign>>();
+            _mockCampaignsUnitOfWork = new Mock<ICampaignsUnitOfWork>();
+            _controller = new CampaignsController(_mockGenericUnitOfWork.Object, _mockCampaignsUnitOfWork.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ShouldReturnOk()
+        {
+            // Arrange
+            var products1 = new List<Product>            {
+                new Product { ProductType = "Activo", Name = "Tarjeta Visa", Quota = 0, Term = "N/A", Value = 0.0, Rate = 0.0 },
+                new Product { ProductType = "Activo", Name = "Tarjeta Mastercard", Quota = 0, Term = "N/A", Value = 0.0, Rate = 0.0 }
+            };
+            var campaigns = new List<Campaign>
+            { new Campaign { Id = 1, CampaignIdentifier = "CP001", CampaignName = "Campaña de prueba", CampaignType = "Tipo 1", Description = "Descripción de prueba", Status = "Asignada", ProductsList = products1, StartDate = DateTime.Now, EndDate = DateTime.Now }};            
+            var response = new ActionResponse<IEnumerable<Campaign>> { WasSuccess = true, Result = campaigns };
+            _mockCampaignsUnitOfWork.Setup(x => x.GetAsync())
+            .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(campaigns, okResult?.Value);
+           _mockCampaignsUnitOfWork.Verify(x => x.GetAsync(), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ShouldReturnError()
+        {
+            // Arrange
+            var response = new ActionResponse<IEnumerable<Campaign>> { WasSuccess = false };
+            _mockCampaignsUnitOfWork.Setup(x => x.GetAsync())
+            .ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync();
+            // Assert
+
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockCampaignsUnitOfWork.Verify(x => x.GetAsync(), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ReturnsOkObjectResult_WhenWasSuccessIsTrue()
+        {   
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new ActionResponse<IEnumerable<Campaign>> { WasSuccess = true, Result = new List<Campaign>() };
+            _mockCampaignsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+
+            //Act
+            var result = await _controller.GetAsync(pagination);
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(response.Result, okResult!.Value);
+            _mockCampaignsUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once()); 
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ReturnsBadRequestResult_WhenWasSuccessIsFalse()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new ActionResponse<IEnumerable<Campaign>> { WasSuccess = false };
+            _mockCampaignsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+            // Act
+            var result = await _controller.GetAsync(pagination);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockCampaignsUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ReturnsOkObjectResult_WhenWasSuccessIsTrue()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var action = new ActionResponse<int> { WasSuccess = true, Result = 5 };
+            _mockCampaignsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(action);
+            
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+            
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(action.Result, okResult!.Value);
+            _mockCampaignsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_ReturnsBadRequestResult_WhenWasSuccessIsFalse()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var action = new ActionResponse<int> { WasSuccess = false };
+            _mockCampaignsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(action);
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockCampaignsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ById_ShouldReturnOk()
+        {
+            // Arrange
+            var campaignId = 1;
+            var products1 = new List<Product>
+            {
+                new Product { ProductType = "Activo", Name = "Tarjeta Visa", Quota = 0, Term = "N/A", Value = 0.0, Rate = 0.0 },
+                new Product { ProductType = "Activo", Name = "Tarjeta Mastercard", Quota = 0, Term = "N/A", Value = 0.0, Rate = 0.0 }
+            };
+            var campaign = new Campaign { Id = campaignId, CampaignIdentifier = "CP001", CampaignName = "Campaña de prueba", CampaignType = "Tipo 1", Description = "Descripción de prueba", Status = "Asignada", ProductsList = products1, StartDate = DateTime.Now, EndDate = DateTime.Now };
+            var response = new ActionResponse<Campaign> { WasSuccess = true, Result = campaign };
+            _mockCampaignsUnitOfWork.Setup(x => x.GetAsync(campaignId)).ReturnsAsync(response);
+
+            // Act
+            var result = await _controller.GetAsync(campaignId);
+            // Assert
+
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(campaign, okResult?.Value);
+            _mockCampaignsUnitOfWork.Verify(x => x.GetAsync(campaignId), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_ById_ShouldReturnNotFound()
+        {
+            // Arrange
+            var campaignId = 1;
+            var response = new ActionResponse<Campaign> { WasSuccess = false, Message = "Not Found" };
+            _mockCampaignsUnitOfWork.Setup(x => x.GetAsync(campaignId)).ReturnsAsync(response);
+            // Act
+            var result = await _controller.GetAsync(campaignId);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundObjectResult));
+            var notFoundResult = result as NotFoundObjectResult;
+            Assert.AreEqual("Not Found", notFoundResult?.Value);
+            _mockCampaignsUnitOfWork.Verify(x => x.GetAsync(campaignId), Times.Once());
+        }
     }
 }
---
+++ Interacoop/InteraCoop.Tests/Controllers/GenericControllerTests.cs
@@ -0,0 +1,106 @@
+﻿using InteraCoop.Backend.Controllers;
+using InteraCoop.Backend.UnitsOfWork.Interfaces;
+using InteraCoop.Shared.Dtos;
+using InteraCoop.Shared.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace InteraCoop.Tests.Controllers
+{
+    [TestClass]
+    public class GenericControllerTests
+    {
+        private Mock<IGenericUnitOfWork<object>> _mockUnitOfWork = null!;
+        private PaginationDTO _paginationDTO = null!;
+        private object _testModel = null!;
+        private int _testId;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockUnitOfWork = new Mock<IGenericUnitOfWork<object>>();
+            _paginationDTO = new PaginationDTO();
+            _testModel = new object();
+            _testId = 1;
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_Success()
+        {
+            // Arrange
+            var response = new ActionResponse<IEnumerable<object>> { WasSuccess = true };
+            _mockUnitOfWork.Setup(x => x.GetAsync(It.IsAny<PaginationDTO>()))
+                .ReturnsAsync(response);
+            var controller = new GenericController<object>(_mockUnitOfWork.Object);
+            // Act
+            var result = await controller.GetAsync(_paginationDTO);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            _mockUnitOfWork.Verify(x => x.GetAsync(It.IsAny<PaginationDTO>()), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Pagination_Failure()
+        {
+            // Arrange
+            var response = new ActionResponse<IEnumerable<object>> { WasSuccess = false };
+            _mockUnitOfWork.Setup(x => x.GetAsync(It.IsAny<PaginationDTO>()))
+                .ReturnsAsync(response);
+            var controller = new GenericController<object>(_mockUnitOfWork.Object);
+            // Act
+            var result = await controller.GetAsync(_paginationDTO);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockUnitOfWork.Verify(x => x.GetAsync(It.IsAny<PaginationDTO>()), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetPagesAsync_Success()
+        {
+            // Arrange
+            var response = new ActionResponse<int> { WasSuccess = true, Result = 5 };
+            _mockUnitOfWork.Setup(x => x.GetTotalPagesAsync(It.IsAny<PaginationDTO>()))
+                .ReturnsAsync(response);
+            var controller = new GenericController<object>(_mockUnitOfWork.Object);
+            // Act
+            var result = await controller.GetPagesAsync(_paginationDTO);
+            // Assert
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(5, okResult.Value);
+            _mockUnitOfWork.Verify(x => x.GetTotalPagesAsync(It.IsAny<PaginationDTO>()), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Id_Success()
+        {
+            // Arrange
+            var response = new ActionResponse<object> { WasSuccess = true, Result = _testModel };
+            _mockUnitOfWork.Setup(x => x.GetAsync(It.IsAny<int>()))
+                .ReturnsAsync(response);
+            var controller = new GenericController<object>(_mockUnitOfWork.Object);
+            // Act
+            var result = await controller.GetAsync(_testId);
+            // Assert
+            var okResult = result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.AreEqual(_testModel, okResult.Value);
+            _mockUnitOfWork.Verify(x => x.GetAsync(It.IsAny<int>()), Times.Once());
+        }
+
+        [TestMethod]
+        public async Task GetAsync_Id_NotFound()
+        {
+            // Arrange
+            var response = new ActionResponse<object> { WasSuccess = false };
+            _mockUnitOfWork.Setup(x => x.GetAsync(It.IsAny<int>()))
+                .ReturnsAsync(response);
+            var controller = new GenericController<object>(_mockUnitOfWork.Object);
+            // Act
+            var result = await controller.GetAsync(_testId);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+            _mockUnitOfWork.Verify(x => x.GetAsync(It.IsAny<int>()), Times.Once());
+        }
+    }
+}
