bed681d8063108ac212c948f015e1c1f30ea03cd
ANDRES CAMILO CASTILLO <castillocamilo17@gmail.com>
2024-06-04 17:55:30 -0500

    Se agregan test controller para get de Clients, Interactions, Productos.

---
+++ Interacoop/InteraCoop.Tests/Controllers/ClientsControllerTests.cs
@@ -0,0 +1,80 @@
+﻿using InteraCoop.Backend.UnitsOfWork.Implementations;
+using InteraCoop.Backend.UnitsOfWork.Interfaces;
+using InteraCoop.Shared.Dtos;
+using InteraCoop.Shared.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InteraCoop.Backend.Controllers
+{
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    public class ClientsController : GenericController<Client>
+    {
+        private readonly IClientsUnitOfWork _clientsUnitOfWork;
+        public ClientsController(IGenericUnitOfWork<Client> unitOfWork, IClientsUnitOfWork clientsUnitOfWork) : base(unitOfWork)
+        {
+            _clientsUnitOfWork = clientsUnitOfWork;
+        }
+
+
+        [HttpGet]
+        public override async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+        {
+            var response = await _clientsUnitOfWork.GetAsync(pagination);
+            if (response.WasSuccess)
+            {
+                return Ok(response.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("totalPages")]
+        public override async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        {
+            var action = await _clientsUnitOfWork.GetTotalPagesAsync(pagination);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("{id}")]
+        public override async Task<IActionResult> GetAsync(int id)
+        {
+            var action = await _clientsUnitOfWork.GetAsync(id);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return NotFound(action.Message);
+        }
+
+        [HttpPost("full")]
+        public async Task<IActionResult> PostFullAsync(ClientDto clientDto)
+        {
+            var action = await _clientsUnitOfWork.AddFullAsync(clientDto);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return NotFound(action.Message);
+        }
+
+        [HttpPut("full")]
+        public async Task<IActionResult> PutFullAsync(ClientDto clientDto)
+        {
+            var action = await _clientsUnitOfWork.UpdateFullAsync(clientDto);
+            if (action.WasSuccess)
+            {
+                return Ok(action.Result);
+            }
+            return NotFound(action.Message);
+        }
+
+    }
+}
+
