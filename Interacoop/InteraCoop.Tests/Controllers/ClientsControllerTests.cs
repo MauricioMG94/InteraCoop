71e312af87f6047fd92d981f8b9077445e71d42a
ANDRES CAMILO CASTILLO <castillocamilo17@gmail.com>
2024-06-04 23:27:10 -0500

    Test - Get Client controller

--- Interacoop/InteraCoop.Tests/Controllers/ClientsControllerTests.cs
+++ Interacoop/InteraCoop.Tests/Controllers/ClientsControllerTests.cs
@@ -1,80 +1,86 @@
-﻿using InteraCoop.Backend.UnitsOfWork.Implementations;
+﻿using InteraCoop.Backend.Controllers;
 using InteraCoop.Backend.UnitsOfWork.Interfaces;
 using InteraCoop.Shared.Dtos;
 using InteraCoop.Shared.Entities;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
+using InteraCoop.Shared.Responses;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
 
-namespace InteraCoop.Backend.Controllers
+namespace InteraCoop.Tests.Controllers
 {
-    [ApiController]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    [Route("api/[controller]")]
-    public class ClientsController : GenericController<Client>
+    [TestClass]
+    public class ClientsControllerTests
     {
-        private readonly IClientsUnitOfWork _clientsUnitOfWork;
-        public ClientsController(IGenericUnitOfWork<Client> unitOfWork, IClientsUnitOfWork clientsUnitOfWork) : base(unitOfWork)
+        private Mock<IGenericUnitOfWork<Client>> _mockGenericUnitOfWork = null!;
+        private Mock<IClientsUnitOfWork> _mockClientsUnitOfWork = null!;
+        private ClientsController _controller = null!;
+
+        [TestInitialize]
+        public void Setup()
         {
-            _clientsUnitOfWork = clientsUnitOfWork;
+            _mockGenericUnitOfWork = new Mock<IGenericUnitOfWork<Client>>();
+            _mockClientsUnitOfWork = new Mock<IClientsUnitOfWork>();
+            _controller = new ClientsController(_mockGenericUnitOfWork.Object, _mockClientsUnitOfWork.Object);
         }
 
-
-        [HttpGet]
-        public override async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+        [TestMethod]
+        public async Task GetAsync_ReturnsOkObjectResult_WhenWasSuccessIsTrue()
         {
-            var response = await _clientsUnitOfWork.GetAsync(pagination);
-            if (response.WasSuccess)
-            {
-                return Ok(response.Result);
-            }
-            return BadRequest();
+            // Arrange
+            var pagination = new PaginationDTO();
+            var response = new ActionResponse<IEnumerable<Client>> { WasSuccess = true, Result = new List<Client>() };
+            _mockClientsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+            //Act
+            var result = await _controller.GetAsync(pagination);
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(response.Result, okResult!.Value);
+            _mockClientsUnitOfWork.Verify(x => x.GetAsync(pagination), Times.Once());
         }
 
-        [HttpGet("totalPages")]
-        public override async Task<IActionResult> GetPagesAsync([FromQuery] PaginationDTO pagination)
+        [TestMethod]
+        public async Task GetAsync_ReturnsBadRequest_WhenWasSuccessIsFalse()
         {
-            var action = await _clientsUnitOfWork.GetTotalPagesAsync(pagination);
-            if (action.WasSuccess)
-            {
-                return Ok(action.Result);
-            }
-            return BadRequest();
+            // Arrange
+            var pagination = new PaginationDTO();
+            // Simulamos un resultado de acción con WasSuccess como falso
+            var response = new ActionResponse<IEnumerable<Client>> { WasSuccess = false };
+            _mockClientsUnitOfWork.Setup(x => x.GetAsync(pagination)).ReturnsAsync(response);
+            // Act
+            var result = await _controller.GetAsync(pagination);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
         }
 
-        [HttpGet("{id}")]
-        public override async Task<IActionResult> GetAsync(int id)
+        [TestMethod]
+        public async Task GetPagesAsync_ReturnsOkObjectResult_WhenWasSuccessIsTrue()
         {
-            var action = await _clientsUnitOfWork.GetAsync(id);
-            if (action.WasSuccess)
-            {
-                return Ok(action.Result);
-            }
-            return NotFound(action.Message);
+            // Arrange
+            var pagination = new PaginationDTO();
+            var action = new ActionResponse<int> { WasSuccess = true, Result = 5 };
+            _mockClientsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(action);
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            var okResult = result as OkObjectResult;
+            Assert.AreEqual(action.Result, okResult!.Value);
+            _mockClientsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
         }
 
-        [HttpPost("full")]
-        public async Task<IActionResult> PostFullAsync(ClientDto clientDto)
+        [TestMethod]
+        public async Task GetPagesAsync_ReturnsBadRequestResult_WhenWasSuccessIsFalse()
         {
-            var action = await _clientsUnitOfWork.AddFullAsync(clientDto);
-            if (action.WasSuccess)
-            {
-                return Ok(action.Result);
-            }
-            return NotFound(action.Message);
+            // Arrange
+            var pagination = new PaginationDTO();
+            var action = new ActionResponse<int> { WasSuccess = false };
+            _mockClientsUnitOfWork.Setup(x => x.GetTotalPagesAsync(pagination)).ReturnsAsync(action);
+            // Act
+            var result = await _controller.GetPagesAsync(pagination);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult));
+            _mockClientsUnitOfWork.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once());
         }
-
-        [HttpPut("full")]
-        public async Task<IActionResult> PutFullAsync(ClientDto clientDto)
-        {
-            var action = await _clientsUnitOfWork.UpdateFullAsync(clientDto);
-            if (action.WasSuccess)
-            {
-                return Ok(action.Result);
-            }
-            return NotFound(action.Message);
-        }
-
     }
 }
-
