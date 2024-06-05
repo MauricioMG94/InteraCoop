382ec5d79b96ff400276c6ac10a33eca8a916407
ANDRES CAMILO CASTILLO <castillocamilo17@gmail.com>
2024-06-04 23:23:36 -0500

    Test - Unit of work of products

--- Interacoop/InteraCoop.Tests/UnitsOfWork/ProductsUnitOfWorkTests.cs
+++ Interacoop/InteraCoop.Tests/UnitsOfWork/ProductsUnitOfWorkTests.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InteraCoop.Backend.Repositories.Interfaces;
+using InteraCoop.Backend.UnitsOfWork.Implementations;
+using InteraCoop.Backend.UnitsOfWork.Interfaces;
+using InteraCoop.Shared.Dtos;
+using InteraCoop.Shared.Entities;
+using InteraCoop.Shared.Responses;
+using Moq;
 
 namespace InteraCoop.Tests.UnitsOfWork
 {
-    internal class ProductsUnitOfWorkTests
+    [TestClass]
+    public class ProductsUnitOfWorkTests
     {
+        private Mock<IGenericRepository<Product>> _mockGenericRepository = null!;
+        private Mock<IProductsRepository> _mockProductsRepository = null!;
+        private ProductsUnitOfWork _unitOfWork = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _mockGenericRepository = new Mock<IGenericRepository<Product>>();
+            _mockProductsRepository = new Mock<IProductsRepository>();
+            _unitOfWork = new ProductsUnitOfWork(_mockGenericRepository.Object, _mockProductsRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAsync_WithPagination_ShouldReturnData()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var expectedResponse = new ActionResponse<IEnumerable<Product>> { WasSuccess = true };
+            _mockProductsRepository.Setup(x => x.GetAsync(pagination))
+            .ReturnsAsync(expectedResponse);
+            // Act
+            var result = await _unitOfWork.GetAsync(pagination);
+            // Assert
+            Assert.AreEqual(expectedResponse, result);
+            _mockProductsRepository.Verify(x => x.GetAsync(pagination), Times.Once);
+        }
+
+
+        [TestMethod]
+        public async Task GetTotalPagesAsync_ShouldReturnTotalPages()
+        {
+            // Arrange
+            var pagination = new PaginationDTO();
+            var expectedResponse = new ActionResponse<int> { WasSuccess = true };
+            _mockProductsRepository.Setup(x => x.GetTotalPagesAsync(pagination))
+            .ReturnsAsync(expectedResponse);
+            // Act
+            var result = await _unitOfWork.GetTotalPagesAsync(pagination);
+            // Assert
+            Assert.AreEqual(expectedResponse, result);
+            _mockProductsRepository.Verify(x => x.GetTotalPagesAsync(pagination), Times.Once);
+        }
     }
 }
